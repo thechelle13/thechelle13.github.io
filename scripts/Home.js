3547c7dc3118a732291758260c4b72ed08fa3d3c
Michelle Totherow <tym4me13@gmail.com>
2024-02-05 09:48:36 -0600

    updates to add navbar

---
+++ scripts/Home.js
@@ -0,0 +1,20 @@
+
+
+function Home() {
+    return `
+        <div class="welcome-container">
+            <h2>Welcome to the Home Page!</h2>
+            <div class="dog-images">
+                <img class="app-logo" src="../assets/IMG_1440.jpeg" alt="Sleepy Doggy" />
+            </div>
+            <div class="welcome-about">
+                <h2>A practice static site built with Github Pages to show a simple deploy.</h2>
+            </div>
+        </div>
+    `;
+}
+
+
+
+
+
---
+++ scripts/NavBar.js
@@ -0,0 +1,28 @@
+
+// function Navbar() {
+//     return `
+//         <nav class="navbar">
+//             <ul>
+//                 <li><a href="Home">Home</a></li>
+//                 <li><a href="About">About</a></li>
+             
+//             </ul>
+//         </nav>
+//     `;
+// }
+
+// Navbar.js
+function Navbar() {
+    return `
+        <nav class="navbar">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </nav>
+    `;
+}
+
+
+
+
