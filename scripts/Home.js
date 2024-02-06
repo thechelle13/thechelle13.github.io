cb411510235268cb90b004bca387cf35b49aa6e9
Michelle Totherow <tym4me13@gmail.com>
2024-02-05 18:05:42 -0600

    update to styling and navbar

--- scripts/Home.js
+++ scripts/Home.js
@@ -8,7 +8,7 @@ function Home() {
                 <img class="app-logo" src="../assets/IMG_1440.jpeg" alt="Sleepy Doggy" />
             </div>
             <div class="welcome-about">
-                <h2>A practice static site built with Github Pages to show a simple deploy.</h2>
+                <h2>A practice static site built with Github Pages to show a deployed site using JS, CSS and HTML.</h2>
             </div>
         </div>
     `;
