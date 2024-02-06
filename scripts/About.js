85a0242e84e59697b15a2efc7511486765ac98c8
Michelle Totherow <tym4me13@gmail.com>
2024-02-05 21:42:40 -0600

    refactor to correct navbar issue

--- scripts/About.js
+++ scripts/About.js
@@ -4,7 +4,7 @@ function About() {
         <div class="welcome-container">
             <h2>About this site:</h2>
             <p>This static site is created using HTML, CSS, and JavaScript, and deployed via GitHub Pages. The journey of production involved challenges, particularly in remembering the basics and understanding how each component communicates.</p>
-            <p>Transitioning to React, a powerful tool for building user interfaces, introduced its own set of challenges. While React offers numerous benefits, such as component reusability and a more organized structure, getting back to the basics and understanding fundamental concepts was crucial.</p>
+            <p>While React offers numerous benefits, such as component reusability and a more organized structure, getting back to the basics and understanding fundamental concepts was the goal of this exercise.</p>
             <p>Despite not utilizing React or other modern frameworks, this site aims to demonstrate the power of traditional web technologies through continuous practice and exploration.</p>
         </div>
     `;
