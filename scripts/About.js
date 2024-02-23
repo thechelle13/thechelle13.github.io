27fb9a9622e836594c852ffbbc5f6666db241b1d
Michelle Totherow <tym4me13@gmail.com>
2024-02-23 16:53:17 -0600

    refactor for readablity and additional styling

--- scripts/About.js
+++ scripts/About.js
@@ -9,18 +9,19 @@ function About() {
             <h2> Current Projects:</h2>
             
             <div>
-                <h3>Weather App:</h3>
-                <p style="font-size: 20px;">A project built in React using JS, HTML and Tailwind to create a working app that fetches the weather based on the zip code entered by the user.  The app then reaches out to RapidAPI to retrieve the weather for that zip code. The app is operational but not deployed. The link to explore the code on GitHub for this project is <a href="https://github.com/thechelle13/weather-client">https://github.com/thechelle13/weather-client</a></p>
+                <h3 style="text-align: left;">Weather App:</h3>
+                <p style="font-size: 20px; text-align: left;">A project built in React using JS, HTML, and Tailwind to create a working app that fetches the weather based on the zip code entered by the user. The app then reaches out to RapidAPI to retrieve the weather for that zip code. The app is operational but not deployed. The link to explore the code on GitHub for this project is <a href="https://github.com/thechelle13/weather-client" style="color: blue; text-decoration: underline;">https://github.com/thechelle13/weather-client</a></p>
             </div>
 
             <div>
-                <h3>Personal Planner Client and API:</h3>
-                <p style="font-size: 20px;">A team collaboration using React, JS, Tailwind, Python and Django to create a Full Stack project. This project is in production and has plans to deploy on Digital Ocean. The GitHub link to explore this project is at <a href="https://github.com/thechelle13/PersonalPlannerClient">https://github.com/thechelle13/PersonalPlannerClient</a>.  Stay tuned for deployment of this app coming soon.</p>
+                <h3 style="text-align: left;">Personal Planner Client and API:</h3>
+                <p style="font-size: 20px; text-align: left;">A team collaboration using React, JS, Tailwind, Python, and Django to create a Full Stack project. This project is in production and has plans to deploy on Digital Ocean. The GitHub link to explore this project is at <a href="https://github.com/thechelle13/PersonalPlannerClient" style="color: blue; text-decoration: underline;">https://github.com/thechelle13/PersonalPlannerClient</a>. Stay tuned for deployment of this app coming soon.</p>
             </div>
 
             <div>
-                <h3>TechPower Client and API:</h3>
-                <p style="font-size: 20px;">A Full Stack project using React, JS, Tailwind, Python and Django to create a modern well-rounded project. This project is in progress of being deployed on Digital Ocean. The GitHub link to the code for the client side is <a href="https://github.com/thechelle13/tech-client">https://github.com/thechelle13/tech-client</a>, and for the API is <a href="https://github.com/thechelle13/tech-api">https://github.com/thechelle13/tech-api</a>. The active deployed Client side is <a href="https://tech-client-er3ag.ondigitalocean.app/">https://tech-client-er3ag.ondigitalocean.app/</a>. However, please note that the backend API is still under development. Once completed, the application will be fully functional. Stay tuned for further updates.</p>
+                <h3 style="text-align: left;">TechPower Client and API:</h3>
+                <p style="font-size: 20px; text-align: left;">A Full Stack project using React, JS, Tailwind, Python, and Django to create a modern well-rounded project. This project is in progress of being deployed on Digital Ocean. The GitHub link to the code for the client side is <a href="https://github.com/thechelle13/tech-client" style="color: blue; text-decoration: underline;">https://github.com/thechelle13/tech-client</a>, and for the API is <a href="https://github.com/thechelle13/tech-api" style="color: blue; text-decoration: underline;">https://github.com/thechelle13/tech-api</a>.</p>
+                <p style="font-size: 20px; text-align: left;">The active deployed site is <a href="https://tech-client-er3ag.ondigitalocean.app/" style="color: blue; text-decoration: underline;">https://tech-client-er3ag.ondigitalocean.app/</a>. However, please note that the site is still under construction. Once completed, the application will be fully functional. Stay tuned for further updates.</p>
             </div>
            
         </div>
