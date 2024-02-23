4dd9131a44126865be1468dace60efc90f33780e
Michelle Totherow <tym4me13@gmail.com>
2024-02-22 18:09:25 -0600

    additonal content and refactor for new navbar link

---
+++ scripts/Resume.js
@@ -0,0 +1,62 @@
+function Resume() {
+    return `
+    <div class="welcome-container">
+        <h2>Michelle Totherow</h2>
+        <p style="font-size: 20px;">Results-driven and detail-oriented Software Developer with a diverse background and a proven track record of successfully transitioning from a thriving sole proprietorship to mastering Full Stack Software Development. I bring hands-on experience in Python, Django, React, and more, coupled with strong project self-management and problem-solving skills. As a former Pawn Broker certified in gun safety and transactions, I showcase meticulous attention to detail. By successfully managing a thriving dog sitting business, I've demonstrated adaptability and a commitment to continuous long-term goals. This background positions me as a versatile and dedicated professional ready to contribute to dynamic software development projects.</p>
+        
+        <h3>Tech Experience</h3>
+        <p style="font-size: 20px;">Postman API · SQLite · Python · Debugging · Front-End Development · React.js · Slack · Terminal · Ubuntu · Git BASH · Visual Studio · GitHub · Zoom · JavaScript · CSS · HTML · Django · MS Teams</p>
+        
+        <h3>Professional Background</h3>
+        
+        <div>
+            <h4>Nashville Software School: Software Developer Apprentice</h4>
+            <p style="font-size: 20px;">Nashville Software School | Feb 2023 - Jan 2024
+            Intensive full-time, six-month software development bootcamp focusing on Python/Django development fundamentals and problem-solving. The final three months of the program were executed in a simulated company environment with SCRUM methodology.
+            
+            - Daily hands-on application of OOP fundamentals through group and individual projects
+            - Created single-page applications in JavaScript, as well as in the React framework
+            - Project management/tracking with GitHub project boards and issue tracking
+            - Source code/version control with Git/GitHub
+            - Solution Design: whiteboarding and wireframes
+            - Leveraged Django for creating and editing server-generated web applications
+            - Created RESTful APIs with Django REST framework
+            - Retrieved and stored database information with SQLite
+            - Practiced testing with Python’s unit test framework</p>
+        </div>
+        
+        <div>
+            <h4>Aunt Chelle’s Doggy DayCare Sole Proprietor</h4>
+            <p style="font-size: 20px;">2016 - 2023
+            - Scheduled appointments often months in advance
+            - Handled logistics to provide services
+            - Marketed services
+            - Managed accounting</p>
+        </div>
+        
+        <div>
+            <h4>EZPAWN SERVICE, S.A. DE C.V. Pawn Broker</h4>
+            <p style="font-size: 20px;">2014 - 2016
+            - Evaluated requested loans
+            - Organized stock
+            - Adhered to guidelines for gun purchases and sales
+            - Assisted in opening and closing store</p>
+        </div>
+        
+        <div>
+            <h4>Ford Credit Customer Service Quality Assurance</h4>
+            <p style="font-size: 20px;">Mar 2000 - Jun 2005
+            - Met and collaborated with international sites to calibrate QA guidelines company-wide
+            - Part of the team responsible for QA guideline rollouts site-wide
+            - Provided one-on-one feedback to Customer Service Agents
+            - Adhered to time management to complete monthly team reviews
+            - Met with Team Leads to provide guidance toward metric improvements</p>
+        </div>
+        
+       
+        
+        
+        
+    </div>
+    `;
+}
--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 const mainContainer = document.querySelector("#container");
 
 // Initially display the Home section
-mainContainer.innerHTML += Navbar() + Home();
+mainContainer.innerHTML += Navbar() + Home(); + Resume();
 
 // Create the Navbar and append it to the mainContainer
 mainContainer.appendChild(Navbar());
