d552242c4f7c159a0fd4d5ff419e8782229b96c2
Michelle Totherow <tym4me13@gmail.com>
2024-09-24 11:01:28 -0500

    update to resume.js

--- scripts/Resume.js
+++ scripts/Resume.js
@@ -1,69 +1,67 @@
 function Resume() {
     return `
     <div class="resume-container">
-    <div class="profile">
-    <h1>Michelle Totherow</h1>
-    <h2>Certified Full Stack Web Developer</h2>
-        <p>Results-driven and detail-oriented Software Developer with a diverse background and a proven track record of successfully transitioning from a thriving sole proprietorship to mastering Full Stack Software Development. I bring hands-on experience in Python, Django, React, and more, coupled with strong project self-management and problem-solving skills. As a former Pawn Broker certified in gun safety and transactions, I showcase meticulous attention to detail. By successfully managing a thriving dog sitting business, I've demonstrated adaptability and a commitment to continuous long-term goals. This background positions me as a versatile and dedicated professional ready to contribute to dynamic software development projects.</p>
-        <p>Connect with me on <a href="https://www.linkedin.com/in/michelletotherow/" target="_blank">LinkedIn</a> or visit my <a href="https://www.michelletotherow.com/" target="_blank">personal website</a>.</p>
+        <div class="profile">
+            <h1>Michelle J. Totherow</h1>
+            <h2>Certified Full Stack Web Developer</h2>
+            <p>Passionate Full Stack Developer with a focus on Python, Django, React, and project self-management. Leveraging a unique background that includes software development, customer service, and entrepreneurial success, I thrive in environments where adaptability, problem-solving, and attention to detail are crucial. With hands-on experience in creating dynamic web applications and managing projects from conception to completion, I am committed to delivering robust, user-focused solutions. Looking for opportunities to make meaningful contributions to development teams that align with my passion for technology and dedication to continuous learning.</p>
+            <p>Connect with me on <a href="https://www.linkedin.com/in/michelletotherow/" target="_blank">LinkedIn</a> or visit my <a href="https://www.michelletotherow.com/" target="_blank">personal website</a>.</p>
+        </div>
+
+        <div class="section">
+            <h3>Tech Experience</h3>
+            <p>Postman API · SQLite · Python · Debugging · Front-End Development · React.js · Slack · Terminal · Ubuntu · Git BASH · Visual Studio · GitHub · Zoom · JavaScript · CSS · HTML · Django · MS Teams</p>
+        </div>
+
+        <div class="section">
+            <h3>Professional Background</h3>
+
+            <div>
+                <h4>Nashville Software School: Software Developer Apprentice</h4>
+                <p>Nashville Software School | Feb 2023 - Jan 2024
+                Intensive full-time, six-month software development bootcamp focusing on Python/Django development fundamentals and problem-solving. The final three months of the program were executed in a simulated company environment with SCRUM methodology.
+                <ul>
+                    <li>Daily hands-on application of OOP fundamentals through group and individual projects</li>
+                    <li>Created single-page applications in JavaScript and React</li>
+                    <li>Project management/tracking with GitHub project boards and issue tracking</li>
+                    <li>Source code/version control with Git/GitHub</li>
+                    <li>Solution Design: whiteboarding and wireframes</li>
+                    <li>Leveraged Django for creating and editing server-generated web applications</li>
+                    <li>Created RESTful APIs with Django REST framework</li>
+                    <li>Retrieved and stored database information with SQLite</li>
+                    <li>Practiced testing with Python’s unit test framework</li>
+                </ul>
+                </p>
+            </div>
+
+            <div>
+                <h4>Aunt Chelle’s Doggy DayCare Sole Proprietor</h4>
+                <p>2016 - 2023
+                - Scheduled appointments often months in advance
+                - Handled logistics to provide services
+                - Marketed services
+                - Managed accounting</p>
+            </div>
+
+            <div>
+                <h4>EZPAWN SERVICE, S.A. DE C.V. Pawn Broker</h4>
+                <p>2014 - 2016
+                - Evaluated requested loans
+                - Organized stock
+                - Adhered to guidelines for gun purchases and sales
+                - Assisted in opening and closing store</p>
+            </div>
+
+            <div>
+                <h4>Ford Credit Customer Service Quality Assurance</h4>
+                <p>Mar 2000 - Jun 2005
+                - Met and collaborated with international sites to calibrate QA guidelines company-wide
+                - Part of the team responsible for QA guideline rollouts site-wide
+                - Provided one-on-one feedback to Customer Service Agents
+                - Adhered to time management to complete monthly team reviews
+                - Met with Team Leads to provide guidance toward metric improvements</p>
+            </div>
+        </div>
     </div>
-
-    <div class="section">
-        <h3>Tech Experience</h3>
-        <p>Postman API · SQLite · Python · Debugging · Front-End Development · React.js · Slack · Terminal · Ubuntu · Git BASH · Visual Studio · GitHub · Zoom · JavaScript · CSS · HTML · Django · MS Teams</p>
-    </div>
-
-    <div class="section">
-        <h3>Professional Background</h3>
-
-        <div>
-            <h4>Nashville Software School: Software Developer Apprentice</h4>
-            <p>Nashville Software School | Feb 2023 - Jan 2024
-            Intensive full-time, six-month software development bootcamp focusing on Python/Django development fundamentals and problem-solving. The final three months of the program were executed in a simulated company environment with SCRUM methodology.
-            <ul>
-                <li>Daily hands-on application of OOP fundamentals through group and individual projects</li>
-                <li>Created single-page applications in JavaScript, as well as in the React framework</li>
-                <li>Project management/tracking with GitHub project boards and issue tracking</li>
-                <li>Source code/version control with Git/GitHub</li>
-                <li>Solution Design: whiteboarding and wireframes</li>
-                <li>Leveraged Django for creating and editing server-generated web applications</li>
-                <li>Created RESTful APIs with Django REST framework</li>
-                <li>Retrieved and stored database information with SQLite</li>
-                <li>Practiced testing with Python’s unit test framework</li>
-            </ul>
-            </p>
-        </div>
-
-        <div>
-            <h4>Aunt Chelle’s Doggy DayCare Sole Proprietor</h4>
-            <p>2016 - 2023
-            - Scheduled appointments often months in advance
-            - Handled logistics to provide services
-            - Marketed services
-            - Managed accounting</p>
-        </div>
-
-        <div>
-            <h4>EZPAWN SERVICE, S.A. DE C.V. Pawn Broker</h4>
-            <p>2014 - 2016
-            - Evaluated requested loans
-            - Organized stock
-            - Adhered to guidelines for gun purchases and sales
-            - Assisted in opening and closing store</p>
-        </div>
-
-        <div>
-            <h4>Ford Credit Customer Service Quality Assurance</h4>
-            <p>Mar 2000 - Jun 2005
-            - Met and collaborated with international sites to calibrate QA guidelines company-wide
-            - Part of the team responsible for QA guideline rollouts site-wide
-            - Provided one-on-one feedback to Customer Service Agents
-            - Adhered to time management to complete monthly team reviews
-            - Met with Team Leads to provide guidance toward metric improvements</p>
-        </div>
-        
-      
-    </div>
-</div>
     `;
 }
